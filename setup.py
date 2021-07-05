29e737e78933218fdbf15e98a41b2f08fe8852a6
nogazin <47559950+nogazin@users.noreply.github.com>
2021-07-05 18:08:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,20 @@ setup(
 
 
 
+# cpa
+from setuptools import setup, find_packages
+
+setup(
+    name='Python-Sample-Application',
+    version='0.0.1',
+    author='Uber Engineering',
+    author_email='developer@uber.com',
+    packages=find_packages(),
+    description='Python sample application',
+)
+
+
+
 
 
 
